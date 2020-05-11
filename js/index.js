e4fa4465fa08fdf8d5164ad99b0a6f2ee7352398
romanptl <63813516+romanptl@users.noreply.github.com>
2020-05-11 16:49:16 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,28 @@
+var oper; //выбранный оператор
+  function func() {
+    var result;
+    var num1 = Number(document.getElementById("num1").value);
+    var num2 = Number(document.getElementById("num2").value);
+    switch (oper) {
+      case '+':
+        result = num1 + num2;
+        break;
+      case '-':
+        result = num1 - num2;
+        break;
+      case '*':
+        result = num1 * num2;
+        break;
+      case '/':
+        if (num2) {
+          result = num1 / num2;
+        } else {
+          result = '';
+        }
+        break;
+      default:
+        result = 'chose';
+    }
+
+    document.getElementById("result").innerHTML = result;
+  }
