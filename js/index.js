b1365900c98251495a74eb3bd4ae666e1dcc544c
romanptl <63813516+romanptl@users.noreply.github.com>
2020-05-13 14:53:27 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,99 @@
-var oper; //выбранный оператор
-  function func() {
-    var result;
-    var num1 = Number(document.getElementById("num1").value);
-    var num2 = Number(document.getElementById("num2").value);
-    switch (oper) {
-      case '+':
-        result = num1 + num2;
-        break;
-      case '-':
-        result = num1 - num2;
-        break;
-      case '*':
-        result = num1 * num2;
-        break;
-      case '/':
-        if (num2) {
-          result = num1 / num2;
-        } else {
-          result = '';
-        }
-        break;
-      default:
-        result = 'chose';
+(function () {
+  "use strict";
+
+  var el = function (element) {
+    if (element.charAt(0) === "#") {
+      return document.querySelector(element);
     }
 
-    document.getElementById("result").innerHTML = result;
+    return document.querySelectorAll(element);
+  };
+  var viewer = el("#viewer"),
+    equals = el("#equals"),
+    nums = el(".num"),
+    ops = el(".ops"),
+    theNum = "",
+    oldNum = "",
+    resultNum,
+    operator;
+
+  var setNum = function () {
+    if (resultNum) {
+      theNum = this.getAttribute("data-num");
+      resultNum = "";
+    } else {
+      theNum += this.getAttribute("data-num");
+    }
+
+    viewer.innerHTML = theNum;
+  };
+
+  var moveNum = function () {
+    oldNum = theNum;
+    theNum = "";
+    operator = this.getAttribute("data-ops");
+
+    equals.setAttribute("data-result", "");
+  };
+
+  var displayNum = function () {
+    oldNum = parseFloat(oldNum);
+    theNum = parseFloat(theNum);
+
+    switch (operator) {
+      case "plus":
+        resultNum = oldNum + theNum;
+        break;
+
+      case "minus":
+        resultNum = oldNum - theNum;
+        break;
+
+      case "times":
+        resultNum = oldNum * theNum;
+        break;
+
+      case "divided by":
+        resultNum = oldNum / theNum;
+        break;
+
+      default:
+        resultNum = theNum;
+    }
+
+    if (!isFinite(resultNum)) {
+      if (isNaN(resultNum)) {
+        resultNum = "You broke it!";
+      } else {
+        resultNum = "Look at what you've done";
+        el("#calculator").classList.add("broken");
+        el("#reset").classList.add("show");
+      }
+    }
+
+    viewer.innerHTML = resultNum;
+    equals.setAttribute("data-result", resultNum);
+
+    oldNum = 0;
+    theNum = resultNum;
+  };
+
+  var clearAll = function () {
+    oldNum = "";
+    theNum = "";
+    viewer.innerHTML = "0";
+    equals.setAttribute("data-result", resultNum);
+  };
+
+  for (var i = 0, l = nums.length; i < l; i++) {
+    nums[i].onclick = setNum;
   }
+
+  for (var i = 0, l = ops.length; i < l; i++) {
+    ops[i].onclick = moveNum;
+  }
+
+  equals.onclick = displayNum;
+
+  el("#clear").onclick = clearAll;
+})();
